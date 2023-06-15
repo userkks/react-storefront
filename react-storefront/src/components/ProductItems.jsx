7faec7402fc80152fdaa3adc07b1d9b211e955de
Koustav <samaddarkoustav@gmail.com>
2023-06-16 03:47:35 +0530

    commif

--- react-storefront/src/components/ProductItems.jsx
+++ react-storefront/src/components/ProductItems.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import watch from "../assets/watch.jpg";
-import { SfButton, SfIconShoppingCart } from "@storefront-ui/react";
+import { SfButton, SfIconFavorite, SfIconShoppingCart } from "@storefront-ui/react";
 
 export default function ProductItems() {
   const productCount = 6;
@@ -14,8 +14,11 @@ export default function ProductItems() {
           {new Array(productCount).fill(1).map((i) => (
             <div key={i}>
               <div className="flex m-3">
-                <div className="default-container h-32 w-32 border border-gray-200 rounded-md">
+                <div className="default-container h-32 w-32 border border-gray-200 rounded-md relative">
                   <img src={watch} alt="Watch" />
+                  <div className=" default-container h-6 w-6 absolute rounded-full border border-purple-600 text-purple-600 bottom-1 right-1">
+                    <SfIconFavorite size="xs"/>
+                </div>
                 </div>
                 <div className=" ml-3 w-52">
                   <div className="flex flex-col justify-between h-full">
@@ -26,7 +29,7 @@ export default function ProductItems() {
                         Heart rate monitor
                       </div>
                     </div>
-                    <div className="flex justify-between">
+                    <div className="flex justify-between items-center">
                         <div className=" font-bold">$2,345.99</div>
                         <SfButton className=" bg-purple-600 hover:bg-purple-800" size="sm"><SfIconShoppingCart/> Add to Cart</SfButton>
                     </div>
